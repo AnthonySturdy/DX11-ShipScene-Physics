164c091250d8913da4d54a5ebe8bac6b23fd8be3
Anthony Sturdy <35777485+AnthonySturdy@users.noreply.github.com>
2020-03-27 00:52:49 +0000

    Created script to auto generate UML content
    Always have to fill it out manually so this should automate it

---
+++ FGGC Framework Semester 2/GenUML.py
@@ -0,0 +1,102 @@
+                  # UML Content Generator #
+#############################################################
+#   PLACE THIS SCRIPT IN THE FOLDER WITH ALL SOURCE FILES   #
+#    IF SOURCE FILES ARE SPLIT INTO FOLDERS, MAKE A COPY    #
+#                 OF FILES IN A NEW FOLDER                  #
+#                                                           #
+#   * Doesn't work on classes where everything is in .h,    #
+#        only works with standard .h files containing       #
+#         Variables, Prototypes and Inline functions        #
+#############################################################
+
+import glob
+
+def main():
+    # For each header file in directory, call ProcessFile()
+    for file in glob.glob("*.h"):
+        ProcessFile(file)
+
+def ProcessFile(fileName):
+    print("Processing " + fileName)
+    file = open(fileName, "r")
+    writeFile = open("GenUML_" + fileName + ".txt", "w+")
+
+    access = ' '        # ' ' = Don't process, '-' = private, '+' = public, '#' = protected
+
+    variables = []
+    functions = []
+
+    # Loop through line by line
+    for line in file:
+        if "public:" in line:
+            access = '+'
+            continue;
+        elif "private:" in line:
+            access = '-'
+            continue;
+        elif "protected:" in line:
+            access = '#'
+            continue;
+        elif "};" in line:
+            access = ' '
+            continue;
+        
+        # If line is to be processed
+        if access != ' ':
+            strippedLine = line
+            # Strip unneeded content from end of line
+            # e.g. will remove '= 25' from 'int x = 25;'
+            #      or '{ return x; } from 'int GetX() { return x; }'
+            if "{" in strippedLine:
+                strippedLine = strippedLine.split('{')[0]
+            if "=" in strippedLine:
+                strippedLine = strippedLine.split('=')[0]
+            if "//" in strippedLine:
+                strippedLine = strippedLine.split("//")[0]
+
+            # Skip if string is empty
+            if strippedLine == "" or strippedLine.isspace():
+                continue;
+
+            # Remove other bits
+            strippedLine = strippedLine.replace(";", "")
+            strippedLine = strippedLine.replace("\n", "")
+            strippedLine = strippedLine.replace("\t", "")
+            strippedLine = strippedLine.replace("const", "")
+            strippedLine = strippedLine.replace("static", "")
+
+            # Remove multiple whitespace
+            strippedLine = " ".join(strippedLine.split())
+
+            # Split string into array
+            lineArray = strippedLine.split(' ')
+
+            # If lineArray length is 1 then must be constructer/destructor, otherwise is variable or function
+            if len(lineArray) > 1:
+                s = access + ' ' + ' '.join(lineArray[:-1]) + ' ' + lineArray[-1]
+                a = s.split(' ')
+                a.insert(2, ":")
+                s2 = " ".join(a)
+                # Check if line is variable or function
+                if '(' in strippedLine:
+                    functions.append(s2)
+                else:
+                    variables.append(s2)
+            else:
+                functions.append(access + ' ' + lineArray[0])
+
+    for x in variables:
+        writeFile.write(x + "\n")
+
+    writeFile.write("\n")
+
+    for x in functions:
+        writeFile.write(x + "\n")
+
+    file.close()
+    writeFile.close()
+            
+            
+
+if __name__ == "__main__":
+    main()
